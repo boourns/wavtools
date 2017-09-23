927ea448b8c4c6d4cd48c08db1bab44670af1a84
Tom Burns <tom.burns@shopify.com>
2017-09-23 17:22:15 -0400

    add split

--- split.sh
+++ split.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 #splits a wav file wherever silence is detected
 
+if [ "$#" != "2" ]; then
+  echo "Usage: $0 <input> <output filename>"
+  exit -1
+fi
+
+sox -V3 "$1" "$2" silence 1 0.50 0.1% 1 0.3 0.1% : newfile : restart
 
